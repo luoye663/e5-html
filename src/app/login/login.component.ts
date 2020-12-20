bf2ef6623e450d533a673fb170dcf4278bcd80d7
APLS <html6@msn.com>
2020-12-20 20:59:49 +0800

    增加一个登录提醒~

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,8 @@ import {NavigationExtras, Route, Router} from '@angular/router';
 import {ActivatedRoute} from '@angular/router';
 import {HttpClientService} from '../service/http-client.service';
 import {NzNotificationService} from 'ng-zorro-antd/notification';
+import {MessageService} from '../service/message.service';
+import {NzModalService} from 'ng-zorro-antd/modal';
 
 @Component({
     selector: 'app-login',
@@ -15,7 +17,7 @@ export class LoginComponent implements OnInit {
     loginUrl = '';
 
     constructor(public route: ActivatedRoute, public router: Router,
-                public http: HttpClientService, private notification: NzNotificationService) {
+                public http: HttpClientService, private notification: NzNotificationService, private modal: NzModalService) {
         route.queryParams.subscribe(value => {
             this.errorText = value.errorText;
             if (value.errorText != null) {
@@ -25,6 +27,13 @@ export class LoginComponent implements OnInit {
         http.get('/auth2/getGithubUrl', {}, value => {
             this.loginUrl = value.data;
             this.isSpin = false;
+            setTimeout(() => {
+                this.modal.create({
+                    nzTitle: '温馨提示',
+                    nzContent: '客官,我看你停留在此页面挺久了，是不是不知道怎么登录？点击 屏幕中间 的小猫猫头像进行登录。',
+                    nzClosable: false
+                });
+            }, 10000);
         });
         // console.log('http返回请求', ht);
     }
